e805ce85e6073ec9b888f9eb32535674c222fde8
Ankur <73573971+AnkurMishra123@users.noreply.github.com>
2020-12-04 11:20:19 +0530

    Create config.py
    I have separated the API tokens

---
+++ config.py
@@ -0,0 +1,3 @@
+""" Attach your telegram and faunadb tokens this is for only testing purpose """
+telegram_bot_token = "1482533514:AAGEOALGFAo1JEAISAT7VK0XaNQDxaXu65Q"
+fauna_secret = "fnAD8KI1XhACDTkH8eXJ1mzDAmlHJMc0vqwlGg1k"
